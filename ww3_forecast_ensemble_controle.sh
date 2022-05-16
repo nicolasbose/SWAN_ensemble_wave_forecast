2dd893abee42ff2d9b16231e782ad7abe7d3d01e
nicolasbose <51712154+nicolasbose@users.noreply.github.com>
2022-05-16 09:47:02 -0300

    Add files via upload

---
+++ ww3_forecast_ensemble_controle.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#####
+# Download forecast data from NOAA:
+# NOAA provide 7 day ahead for GFS Wind ensemble
+#####
+
+
+data_name=20210926
+
+
+
+mkdir 'wave_c_'$data_name
+
+cd 'wave_c_'$data_name/
+
+
+## time = 0 - 9
+for i in $(seq 0 3 9)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.c00.global.0p25.f00'$i'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+data='ww3_c_forecast_'$data_name'_f00'$i'.grb2'
+
+curl -o $data $url_gfs
+
+done
+
+
+## time = 12 - 99
+for n in $(seq 12 3 99)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.c00.global.0p25.f0'$n'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+data='ww3_c_forecast_'$data_name'_f0'$n'.grb2'
+
+curl -o $data $url_gfs
+
+done
+
+## time = 102 - 180
+for j in $(seq 102 3 240)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.c00.global.0p25.f'$j'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+data='ww3_c_forecast_'$data_name'_f'$j'.grb2'
+
+curl -o $data $url_gfs
+
+done
---
+++ ww3_forecast_ensemble_members_1.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#####
+# Download forecast data from NOAA:
+# NOAA provide 7 day ahead for GFS Wind ensemble
+#####
+
+
+data_name=20210927
+
+
+
+mkdir 'wave_p_'$data_name
+
+cd 'wave_p_'$data_name/
+
+
+for p in $(seq 1 1 9)
+do
+
+## time = 0 - 9
+for i in $(seq 0 3 9)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.p0'$p'.global.0p25.f00'$i'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+
+data='ww3_0'$p'_forecast_'$data_name'_f00'$i'.grb2'
+
+curl -o $data $url_gfs
+
+done
+
+
+## time = 12 - 99
+for n in $(seq 12 3 99)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.p0'$p'.global.0p25.f0'$n'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+data='ww3_0'$p'_forecast_'$data_name'_f0'$n'.grb2'
+
+curl -o $data $url_gfs
+
+done
+
+## time = 102 - 180
+for j in $(seq 102 3 240)
+do
+
+url_gfs='https://nomads.ncep.noaa.gov/cgi-bin/filter_gefs_wave_0p25.pl?file=gefs.wave.t00z.p0'$p'.global.0p25.f'$j'.grib2&lev_surface=on&var_DIRPW=on&var_HTSGW=on&var_PERPW=on&subregion=&leftlon=148&rightlon=156&toplat=-30&bottomlat=-38&dir=%2Fgefs.'$data_name'%2F00%2Fwave%2Fgridded'
+
+data='ww3_0'$p'_forecast_'$data_name'_f'$j'.grb2'
+
+curl -o $data $url_gfs
+
+done
+
+done
